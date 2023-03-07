f0ae4006779d6bbcc0510726465bc784abc8a94d
Hanif Naufal Rafandi <rafandinaufal03@gmail.com>
2023-03-07 13:30:47 +0700

    Project AdminLTE

--- resources/views/pengalamanKuliah.blade.php
+++ resources/views/pengalamanKuliah.blade.php
@@ -51,7 +51,7 @@
 					<div class="direct-chat-msg">
 						<div class="direct-chat-infos clearfix">
 							<span class="direct-chat-name float-left">Hanif Naufal Rafandi</span>
-							<span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
+							<span class="direct-chat-timestamp float-right">hdjjjab</span>
 						</div>
 						<!-- /.direct-chat-infos -->
 						<img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
